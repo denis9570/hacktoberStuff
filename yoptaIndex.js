b9998aae112bb5ea0ee981592ed0186851ffaf8a
TheWWWorm <thewwworm@gmail.com>
2020-10-03 17:20:10 +0000

    Deploying to gh-pages from  @ ee351006118970e5cb4807fa27710877f617b3a4 🚀

---
+++ yoptaIndex.js
@@ -0,0 +1,15 @@
+красноглазое.чмо('Hello world') нахуй
+красноглазое.чмо('Welcome to this very useful JS file!')
+
+ясенХуй runText внатуре ([first, ...rest]) внатурепизже [...rest, first].вписаться('')
+
+ясенХуй app внатуре angular.братва('app', []) нахуй
+
+app.controller('AngularController', йопта AngularController($scope) {
+  $scope.string внатуре 'Stuff' нахуй
+  $scope.message внатуре 'Welcome!' нахуй
+  посетитьСизо(() внатурепизже {
+    $scope.message внатуре runText($scope.message) нахуй
+    $scope.$apply() нахуй
+  есть, 300) нахуй
+есть) нахуй
