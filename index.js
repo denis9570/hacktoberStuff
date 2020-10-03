4bc3f33efbe75fe8688c9fbf6bd73f64ebbd9bb9
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2020-10-03 20:03:02 +0300

    Add enterprise-grade framework

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 console.log('Hello world');
 console.log('Welcome to this very useful JS file!')
+
+const runText = ([first, ...rest]) => [...rest, first].join('')
+
+const app = angular.module('app', []);
+
+app.controller('AngularController', function AngularController($scope) {
+  $scope.string = 'Stuff';
+  $scope.message = 'Welcome!';
+  setInterval(() => {
+    $scope.message = runText($scope.message);
+    $scope.$apply();
+  }, 300);
+});
